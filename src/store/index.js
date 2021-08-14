78c52891d54b2f7c8dc011243cf66852b4c8f829
mjocc <61629073+mjocc@users.noreply.github.com>
2021-08-14 11:27:14 +0100

    Start adding state

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,20 @@
 import { createStore } from "vuex";
 
+import mutations from "./mutations.js";
+import actions from "./actions.js";
+
+import airports from "./modules/airports.js";
+import aircraft from "./modules/aircraft.js";
+import flightplans from "./modules/flightplans.js";
+
 export default createStore({
   state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  mutations,
+  actions,
+  modules: {
+    airports,
+    aircraft,
+    flightplans,
+  },
+  strict: process.env.NODE_ENV !== "production",
 });
---
+++ src/store/modules/airports.js
@@ -0,0 +1,34 @@
+export default {
+  state: {
+    JFK: {
+      code: "JFK",
+      name: "",
+      distanceFromLPL: "",
+      distanceFromBOH: "",
+    },
+    ORY: {
+      code: "ORY",
+      name: "",
+      distanceFromLPL: "",
+      distanceFromBOH: "",
+    },
+    MAD: {
+      code: "MAD",
+      name: "",
+      distanceFromLPL: "",
+      distanceFromBOH: "",
+    },
+    AMS: {
+      code: "AMS",
+      name: "",
+      distanceFromLPL: "",
+      distanceFromBOH: "",
+    },
+    CAI: {
+      code: "CAI",
+      name: "",
+      distanceFromLPL: "",
+      distanceFromBOH: "",
+    },
+  },
+};
